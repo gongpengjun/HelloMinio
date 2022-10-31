20c256824ec143db9c8697940b07fa030cc6bab6
gongpengjun <frank.gongpengjun@gmail.com>
2022-10-31 14:16:51 +0800

    init commit create project by IDEA Spring Initializr

---
+++ src/test/java/com/gongpengjun/hellominio/HelloMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongpengjun.hellominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMinioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
